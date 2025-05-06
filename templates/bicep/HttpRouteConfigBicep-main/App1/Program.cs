aa07bd848c75a9f60e2a5df691cf5c8731556548
Liana Adaza <52001041+LianaAdaza@users.noreply.github.com>
2025-05-06 11:03:07 -0700

    Delete templates/bicep/HttpRouteConfigBicep-main directory

--- templates/bicep/HttpRouteConfigBicep-main/App1/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "App1");
-
-app.Run();
--- templates/bicep/HttpRouteConfigBicep-main/App2/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "App2");
-
-app.Run();
